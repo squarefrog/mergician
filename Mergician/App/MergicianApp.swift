a05e7ef3b1e5a6c144f06615b2ed8e19e38b90e8
Paul Williamson <squarefrog@gmail.com>
2021-11-05 18:29:43 +0000

    Initial commit

---
+++ Mergician/App/MergicianApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Paul Williamson on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MergicianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello world")
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Mergician
+
+Mergician is a macOS menu bar application that allows you to see open merge requests for a given project.
+
+## System Requirements
+
+- macOS 11.0 Big Sur, or above
+
