99f64063fbd184eff12aae2b4dcbeaa7a2d25e75
Paul Williamson <squarefrog@gmail.com>
2021-11-11 19:19:16 +0000

    Add local path to settings

--- Mergician/Settings/SettingsView.swift
+++ Mergician/Settings/SettingsView.swift
@@ -3,12 +3,20 @@ import SwiftUI
 struct SettingsView: View {
     @State private var accessToken: String = ""
     @State private var projectURL: String = ""
+    @State private var folderPath: String = ""
 
     var body: some View {
         Form {
             SecureField("Access Token", text: $accessToken)
 
             TextField("Project URL", text: $projectURL)
+
+            HStack {
+                TextField("Local Path", text: $folderPath)
+                Button(action: chooseFolder) {
+                    Text("Choose")
+                }
+            }
 
             HStack {
                 Spacer()
@@ -26,6 +34,16 @@ struct SettingsView: View {
         }
 
         .padding()
+    }
+
+    func chooseFolder() {
+        let panel = NSOpenPanel()
+        panel.canChooseFiles = false
+        panel.canChooseDirectories = true
+
+        if panel.runModal() == .OK {
+            folderPath = panel.url?.path ?? ""
+        }
     }
 }
 
