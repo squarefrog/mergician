8c37b40ef2b2418fdbf023941fd53d834d1421b2
Paul Williamson <squarefrog@gmail.com>
2021-11-14 18:39:20 +0000

    Rename DateFormatter

---
+++ Mergician/Extensions/DateFormatter+relative.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension DateFormatter {
+    static let relative: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        formatter.doesRelativeDateFormatting = true
+        return formatter
+    }()
+}
