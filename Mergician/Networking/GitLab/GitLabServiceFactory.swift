dcbc3738e76cf84c113c856a48a29c165c13dbe0
Paul Williamson <squarefrog@gmail.com>
2021-12-19 14:59:24 +0000

    GitLabServiceFactory should return abstract class

--- Mergician/Networking/GitLab/GitLabServiceFactory.swift
+++ Mergician/Networking/GitLab/GitLabServiceFactory.swift
@@ -2,7 +2,8 @@ import Foundation
 import GitLab
 
 enum GitLabServiceFactory {
-    static func makeService(token: String, projectURL: URL) throws -> GitLabServiceProtocol {
-        try GitLabService(token: token, projectURL: projectURL, urlSession: URLSession.shared)
+    static func makeService(token: String, projectURL: URL) throws -> NetworkServiceProviding {
+        let service = try GitLabService(token: token, projectURL: projectURL, urlSession: URLSession.shared)
+        return GitLabServiceAdapter(service: service)
     }
 }
